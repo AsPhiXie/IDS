fb66cb37d6c1bf066c80ffe4839de24c2ea140b2
micleb <lebreton.mickael@protonmail.com>
2018-11-30 16:04:27 +0100

    update https

---
+++ cti_https.py
@@ -0,0 +1,36 @@
+import socket
+import ssl
+import OpenSSL
+from pprint import pprint
+import sys
+import requests
+
+# https://23.253.135.79/
+
+if len(sys.argv) < 2:
+    print("Error, argument required")
+    exit(-1)
+
+HOST = sys.argv[1]
+PORT = 443
+
+# cert = ssl.get_server_certificate((HOST, PORT))
+# x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+# pprint(x509.get_subject().get_components())
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((HOST, PORT))
+# s = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_SSLv23)
+# s.send("GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(HOST).encode())
+# print(s)
+# s.close()
+
+# catcher d'autres exceptions
+try:
+    r = requests.get(HOST, verify=True)
+    print(r)
+except requests.exceptions.SSLError:
+    print("SSL ERROR BAD CERTIFICATE DOMAIN")
+
+
+# L'api circl pour AS
