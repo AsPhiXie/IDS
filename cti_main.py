b9ddc7a612f7953dbe2719d83356e42260d3a40b
AsPhiXie <sina.khoubi@etudiant.univ-rennes1.fr>
2018-12-06 16:25:28 +0100

    Fonctionnel sans WHOIS

--- cti_main.py
+++ cti_main.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 import logging
-import sys
 import datetime
 import certstream
 import requests
+from stix2 import Indicator
 
 certstream_url = 'wss://certstream.calidog.io'
 score = 100
@@ -28,33 +28,43 @@ def checkHTTPS(url):
     except Exception:
         #print("SSL ERROR BAD CERTIFICATE DOMAIN")
         score = score - 50
-        print("Site non accepté. URL = " + url)
-        print("Score = " + str(score))
+        i = STIX2(url, score, "Site sans certificat SSL.")
+        print(i)
+        return
+        #print("Site non accepté. URL = " + url)
+        #print("Score = " + str(score))
 
 def verif_whois(url):
     return
 
 def analyseVisite(url):
+    global score
     r = requests.get("https://www.alexa.com/siteinfo/" + url)
     page = str(r.text)
     if 'We don\'t have enough data to rank this website.' in page:
-        print("Aucune donnée sur ce domaine.")
-        return -1
+        score = score - 50
+        #print("Aucune donnée sur ce domaine.")
+        i = STIX2(url, score, "Pas de donnee Alexa.")
+        print(i)
+        return
     else:
         #page = str(r.text).split("demographics_div_country_table")[1].split("data-count")[1].split("&nbsp;")[1].split("</a>")[0]
         globalRank = page.split("<!-- Alexa web traffic metrics are available via our API at http://aws.amazon.com/awis -->\n")[1].split(' ')[0]
         country = page.split("countryRank")[2].split("title=\'")[1].split("\'")[0]
         countryRank = page.split("metrics-data align-vmiddle\">\n")[2].split(" ")[0]
-        print("Rang mondial : " + globalRank)
-        print("Pays : " + country)
-        print("Rang dans le pays : " + countryRank)
-        return 0
-
-def verif_herbergeur_geoloc(url):
-    return
-
-def filtre():
-    return
+        #print("Rang mondial : " + globalRank)
+        intGLobal = int(globalRank)
+        if (intGLobal > 4000000):
+            score -= 40
+        elif (intGLobal > 3000000):
+            score -= 30
+        elif (intGLobal > 2000000):
+            score -= 20
+        elif (intGLobal > 1000000):
+            score -= 10
+        i = STIX2(url, score, "Rang mondiale = " + globalRank)
+        print(i)
+        return
 
 def traitementURL(domain):
     motcle =  ["banque", "credit", "agricole", "mutuel", "lcl.", "lyonnais", "bforbank", "hellobank", "bank", "caisse", "epargne", "hsbc", "fortuneo", "axa.", "groupama",
@@ -63,10 +73,11 @@ def traitementURL(domain):
                "standardchartered","santander", "nationwide","nordea","skandia", ]
     for mot in motcle:
         if mot in domain:
-            sys.stdout.write(u"[{}] {} \n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain))
-            sys.stdout.flush()
-    return trustedSite(domain)
+            return trustedSite(domain)
 
+def STIX2(url, score, description):
+    indik = Indicator(name="URL Suspecte", labels=["malicious-activity"], created=datetime.datetime.now(), description="Score = " + str(score) + " | " + description, pattern="[url:value = '" + url + "']")
+    return indik
 
 def print_callback(message, context):
     global score
@@ -79,7 +90,6 @@ def print_callback(message, context):
         domain = "NULL"
     else:
         domain = all_domains[0]
-        print(domain)
         score = 100
         traitementURL(domain)
 
