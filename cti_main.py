79ed1df4450f4e32083a41df1a872d2342b36db5
micleb <lebreton.mickael@protonmail.com>
2018-12-06 19:32:01 +0100

    update whois

--- cti_main.py
+++ cti_main.py
@@ -4,9 +4,70 @@ import datetime
 import certstream
 import requests
 from stix2 import Indicator
+import whois
+import datetime
 
 certstream_url = 'wss://certstream.calidog.io'
 score = 100
+bl_dns = {
+    "all.s5h.net",
+    "b.barracudacentral.org",
+    "bl.emailbasura.org",
+    "bl.spamcannibal.org",
+    "bl.spamcop.net",
+    "blacklist.woody.ch",
+    "bogons.cymru.com",
+    "cbl.abuseat.org",
+    "cdl.anti-spam.org.cn",
+    "combined.abuse.ch",
+    "db.wpbl.info",
+    "dnsbl-1.uceprotect.net",
+    "dnsbl-2.uceprotect.net",
+    "dnsbl-3.uceprotect.net",
+    "dnsbl.anticaptcha.net",
+    "dnsbl.dronebl.org",
+    "dnsbl.inps.de",
+    "dnsbl.sorbs.net",
+    "dnsbl.spfbl.net",
+    "drone.abuse.ch",
+    "duinv.aupads.org",
+    "dul.dnsbl.sorbs.net",
+    "dyna.spamrats.com",
+    "dynip.rothen.com",
+    "http.dnsbl.sorbs.net",
+    "ips.backscatterer.org",
+    "ix.dnsbl.manitu.net",
+    "korea.services.net",
+    "misc.dnsbl.sorbs.net",
+    "noptr.spamrats.com",
+    "orvedb.aupads.org",
+    "pbl.spamhaus.org",
+    "proxy.bl.gweep.ca",
+    "psbl.surriel.com",
+    "relays.bl.gweep.ca",
+    "relays.nether.net",
+    "sbl.spamhaus.org",
+    "short.rbl.jp",
+    "singular.ttk.pte.hu",
+    "smtp.dnsbl.sorbs.net",
+    "socks.dnsbl.sorbs.net",
+    "spam.abuse.ch",
+    "spam.dnsbl.anonmails.de",
+    "spam.dnsbl.sorbs.net",
+    "spam.spamrats.com",
+    "spambot.bls.digibase.ca",
+    "spamrbl.imp.ch",
+    "spamsources.fabel.dk",
+    "ubl.lashback.com",
+    "ubl.unsubscore.com",
+    "virus.rbl.jp",
+    "web.dnsbl.sorbs.net",
+    "wormrbl.imp.ch",
+    "xbl.spamhaus.org",
+    "z.mailspike.net",
+    "zen.spamhaus.org",
+    "zombie.dnsbl.sorbs.net"
+}
 
 def trustedSite(url):
     global score
@@ -24,7 +85,7 @@ def checkHTTPS(url):
     try:
         requests.get("https://" + url, verify=True)
         #print("SSL GOOD CERTIFICATE DOMAIN")
-        return analyseVisite(url)
+        return verif_whois(url)
     except Exception:
         #print("SSL ERROR BAD CERTIFICATE DOMAIN")
         score = score - 50
@@ -33,7 +94,20 @@ def checkHTTPS(url):
         return
 
 def verif_whois(url):
-    return
+    global score
+    domain = whois.whois(NAME)
+    cd = domain.creation_date
+    td = datetime.datetime.now()
+
+    for server in domain.name_servers:
+        if server in bl_dns:
+            score -= -50
+            return
+
+    if (td - cd).days < 365:
+        score -= 25
+
+    return analyseVisite(url)
 
 def analyseVisite(url):
     global score
@@ -97,4 +171,3 @@ def print_callback(message, context):
 
 logging.basicConfig(format='[%(levelname)s:%(name)s] %(asctime)s - %(message)s', level=logging.INFO)
 certstream.listen_for_events(print_callback, url=certstream_url)
-
